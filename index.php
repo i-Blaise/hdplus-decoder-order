189db1b33559cb8c17349abf3dfd00c63dbad75a
unknown <creativ.drops@gmail.com>
2022-04-26 17:21:46 +0000

    hdplus-decoder-order

--- index.php
+++ index.php
@@ -112,16 +112,16 @@
           <!-- This Mother’s Day, it’s your turn to put smiles on Mom’s face as she watches her favourite shows FeeliFeeli in 5X clearer pictures.
 
           The first 3 orders per day get to treat mom to lunch with Adina and Akrobetu on Mother’s Day. -->
-      <h3 class="main-page-header">This Mother’s Day</h3>
-		  <p class="main-page-text">it’s your turn to put smiles on Mom’s face as she watches her favourite shows FeeliFeeli in 5X clearer pictures. <br> The first 3 orders per day get to treat mom to lunch with Adina and Akrobetu on Mother’s Day.</p>
+		  <p class="main-page-text" style="padding-top: 40px; padding-bottom: 10px;">This mother’s day, it’s your turn to put smiles on Mom’s face as she <br> watches her favourite shows FeeliFeeli in 5X clearer pictures.</p>
+      <p class="main-page-text">The first 3 orders per day get to treat mom to <br> lunch with Adina and Akrobetu on Mother’s Day.</p>
   
             <div class="container-login100-form-btn">
               <a class="btn" href="order-decoder/"><button class="login100-form-btn" name="submit" value="Submit">
                 Gift mom a decoder
               </button> </a>
-              <a class="btn" href="?status=notopen" id="subscription" disabled><button class="login100-form-btn" name="submit" value="Submit">
+              <!-- <a class="btn" href="?status=notopen" id="subscription" disabled><button class="login100-form-btn" name="submit" value="Submit">
                 Gift Mom a Subscription
-              </button></a>
+              </button></a> -->
             </div>
         </div>
       </div>
